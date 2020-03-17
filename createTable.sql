567b5044548e5b31e4d75f7f514c9b661d890016
unknown <sindrisa@gmail.com>
2020-03-17 12:24:52 +0000

    first commit

--- createTable.sql
+++ createTable.sql
@@ -1,21 +1,10 @@
 CREATE TABLE Kef_main (
-    Timestamp           timestamp,
-    Site                varchar(255),
-    Workstation_name    varchar(255),
-    Terminal            varchar(255),
-    Checkpoint          varchar(255),
-    Scanning_position   varchar(255),
-    Flight_number       int,
-    Airline             varchar(255),
-    Destination         varchar(255),
-    TravelDate          timestamp,
-    STD                 time,
-    Boardingpasshash    varchar(255),
-    Gate                varchar(255)
-);
+    personid            varchar(255),
+    area                varchar(255),
+    timestamp           timestamp,
+    entertime           timestamp,
+    leavetime           timestamp,
+    totaltime           int,
+    flightname          varchar(255),
 
-CREATE TABLE Scanner_pos (
-    Scanning_position   varchar(255),
-    x_coord     int,
-    y_coord     int
 );
